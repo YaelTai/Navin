5fc3f93ff5a5e5cb7ad585d1e16cadabc276b16e
YaelTai <36214121865@mby.co.il>
2023-03-20 11:28:50 +0200

    fhf

--- client/src/conponents/user_home.js
+++ client/src/conponents/user_home.js
@@ -19,30 +19,37 @@ const UserHome = () => {
 
         </div>
     );
-   
+
     return (
         <Card title="Welcome!" footer={footer} header={header} className="md:w-25rem" style={{ "margin": "2%", "width": "95%", "height": "95%" }}>
             <p className="m-0">
                 <h4>Please take a picture of the store sign to your right:</h4>
                 {/* <iframe src="https://example.com/camera-pro-iframe" allow="camera;"/> */}
 
-                {!image ? <Camera ref={camera} aspectRatio={8 / 5} /> :
-                    <img src={image} alt='Taken photo' style={{ "width": "70%" }} />}
+                {!image ? <><Camera ref={camera} aspectRatio={8 / 5} />
+                    <Button label="take a picture" icon="pi-camera" onClick={() => {
+                       
+                        setImage(camera.current.takePhoto())
 
-                <Button label="take a picture" icon="pi-camera" onClick={() => {
-                    setFlag(true)
-                    setImage(camera.current.takePhoto())
+                    }} />
+                </> : <>
 
-                }} />
-                  {!image ?    <></>: <>
-            <h4>you are near:  XXXX
-            </h4>
-            <span className="p-buttonset">
-                <Button label="Save" icon="pi pi-check" />
-                <span>    </span>
-                <Button label="Cancel" icon="pi pi-times" />
-            </span></> 
-                   }
+                    <img src={image} alt='Taken photo' style={{ "width": "70%" }} />
+
+                    <h4>you are near:  XXXX </h4>
+
+                    <span className="p-buttonset">
+                        <Button label="Save" icon="pi pi-check" />
+                        <br/> <br/>
+                        <Button label="Canceling and reshooting" icon="pi pi-times" onClick={() => {
+                       
+                        setImage("")
+
+                    }} />
+                    </span></>
+                }
+
+
             </p>
         </Card>
 
